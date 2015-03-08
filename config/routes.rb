51c014dfc9cf276626c13808a65e37d4d6faef6f
realli <lrlanswer@gmail.com>
2015-03-08 15:56:51 +0800

    it G

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   get 'say/hello'
 
